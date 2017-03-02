6aaf64c58ce301ca8e0b21f65d5c0d88fb924c11
Shachindra <shachindra.spidey@gmail.com>
2017-03-02 22:08:06 +0530

    A few Modifications

--- Tracking/Color_Track.py
+++ Tracking/Color_Track.py
@@ -1,3 +1,8 @@
+#Steps to get HSV Boundary Values
+#color = np.uint8([[[0, 128, 0]]])
+#hsv_green = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
+#print (hsv_color)
+
 import cv2
 import numpy as np
 print("OpenCV Version: "+cv2.__version__)
@@ -12,11 +17,17 @@ while(True):
 	#Convert BGR to HSV
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	#define range of blue color in HSV
+	#define range of colors in HSV
+	lower_red = np.array([-10, 50, 50])
+	upper_red = np.array([20, 255, 255])
 	lower_blue = np.array([110, 50, 50])
 	upper_blue = np.array([130, 255, 255])
-	
-	#Threshold the HSV image to get only blue colors
+	lower_yellow = np.array([20, 50, 50])
+	upper_yellow = np.array([40, 255, 255])
+	lower_green = np.array([50, 50, 50])
+	upper_green = np.array([70, 255, 128])
+
+	#Threshold the HSV image to get only the specified colors
 	mask = cv2.inRange(hsv, lower_blue, upper_blue)
 	
 	#Bitwise-AND mask and original image
