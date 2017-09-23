d867a4871d92bddc3ca57a2f1a5f7cf1b755f3c1
Shachindra <shachindra.spidey@gmail.com>
2017-09-23 18:18:27 +0530

    Wait for Upcoming Tutorials

--- TakePic/TakePic1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-cap = cv2.VideoCapture(0)
-
-#cv2.NamedWindow("webcam", 1)
-#capture = cv2.CaptureFromCAM(-1)
-img = cv2.QueryFrame(capture)
-cv2.SaveImage("CamPics/cameraPic01.jpg", img)
